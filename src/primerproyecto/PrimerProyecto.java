9cfea55732d76fbd9e455d9f3b57072580c6d97c
Brian Bellagamba <bellagambabrian@yahoo.com.ar>
2023-03-25 12:01:10 -0300

    Primer Commit

---
+++ src/primerproyecto/PrimerProyecto.java
@@ -0,0 +1,13 @@
+package primerproyecto;
+
+
+public class PrimerProyecto {
+
+    public static void main(String[] args) {
+       int num1 = 4;
+       int num2 = 6;
+       int suma = num1 + num2;
+       System.out.println ("Esta es la suma de num1 y num2: " + suma);
+    }
+    
+}
